76245ea45857627c41c13b7a2b9000466fbfb04b
David Mix <mixelectronico@gmail.com>
2024-08-18 02:23:57 -0400

    remake of the main JS file. Classes and Objects are well Donegit add .

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-function Prooducto(nombre, precio){
-    let _nombre = nombre;
-    let _precio = precio;
 
-    Object.defineProperties(this, {
-        nombre:{
-            get: function(){
-                return _nombre.toUpperCase();
-            },
-            set: function(new_name){
-                _nombre = new_name;
-            }
-        },
-        precio:{
-            get: function(){
-                return _precio();
-            },
-            set: function(new_price){
-                _precio = new_price;
-            }
-        }
-    })
+// ------------ DECLARACION DE CLASES ----------------------
+
+class Producto{
+    Constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 }
+
+class Carrito{
+    carrito = [];
+
+    añadirProducto(producto){
+        this.carrito.push(producto);
+    };
+}
+
+// ------------ DECLARACION DE OBJETOS ----------------------
+
+const pilaMediana = new Producto('Pila AA', 2000);
+const pilaChica   = new Producto('Pila AAA',1000);
+const linterna    = new Producto('Linterna a Pilas AA',8000);
+const radioPila   = new Producto('Radio Personal',12000);
+const linternaRec = new Producto('Linterna recargable',20000);
+
+let tienda = new Carrito();
+
+// ------------ INICIO DE RUTINA ----------------------
